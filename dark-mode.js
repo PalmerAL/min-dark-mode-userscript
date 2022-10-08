1da4d59f5287755d911d32c31f40af7dd4c4dc6b
Sandy Lim <chocopie@users.noreply.github.com>
2022-10-08 21:13:08 +0800

    Update dark-mode.js
    More aggressive and pedantic darkmoding of HTML page elements.

--- dark-mode.js
+++ dark-mode.js
@@ -8,18 +8,30 @@ var style = document.createElement('style')
 
 style.textContent = `
   html {
-    background-color: #ddd;
-    filter: hue-rotate(180deg) invert(100%) !important;
+    background-color: #000 !important;
+    background-image:none !important;
   }
   body {
-    margin: 0;
-    background-color: #ddd;
+    background-color: #000 !important;
     /* twitter.com */
     min-height: 100vh;
+    background-image:none !important;
+  }
+  header, nav, div, span, pre, h1, h2, h3, blockquote, table, tr, th, td, article, section, textarea {
+    background-color: #000 !important;
+    background-image:none !important;
+    color: #ddd !important;
+  }
+  p, li, span, pre, code, blockquote, td, b, i, u {
+    color: #ddd !important;
+  }
+  a {
+    color: #E87E65 !important;
+    text-decoration: underline !important;
   }
   
   iframe, img, video, canvas {
-  filter: hue-rotate(180deg) invert(100%) !important;
+  filter: brightness(0.5) contrast(1.5) !important;
   }
   `
 
